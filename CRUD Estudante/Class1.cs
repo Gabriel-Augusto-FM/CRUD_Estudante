17d3fc2673cbee6d3257b66d3006fa9d3100f831
Gabriel Augusto <gabriel.cursoaugustus@gmail.com>
2024-11-01 16:26:35 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD Estudante/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_Estudante
+{
+    public class Estudante
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int CPF { get; set; }
+    }
+}
