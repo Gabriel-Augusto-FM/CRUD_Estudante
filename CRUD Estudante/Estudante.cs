ad7b8bf030ee09c2893e0fddfbbcaf4f195f5e7e
Gabriel Augusto <gabriel.cursoaugustus@gmail.com>
2024-11-04 17:22:10 -0300

    Listar e Adicionar

--- CRUD Estudante/Estudante.cs
+++ CRUD Estudante/Estudante.cs
@@ -6,5 +6,10 @@
         public string Nome { get; set; }
         public int Idade { get; set; }
         public int CPF { get; set; }
+
+        internal List<Estudante> Listar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
